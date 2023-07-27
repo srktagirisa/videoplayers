63bf12110355524cb6f11470a5f230f4048041c6
sivatagirisa <45546263+srktagirisa@users.noreply.github.com>
2023-07-26 20:04:16 -0700

    Collapsible component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Collapsible from './components/Collapsible';
 import WindowSizeDisplay from './components/WindowSizeDisplay';
 import DeviceType from './components/DeviceType';
 
+
 function App() {
+
   return (
     <div className="App">
       <WindowSizeDisplay></WindowSizeDisplay>
       <DeviceType></DeviceType>
       <Collapsible title="React">
         The library for web and native user interfaces
+      </Collapsible>
+      <Collapsible title="States and Props">
+        The summary of States and Props
+      </Collapsible>
+      <Collapsible title="React Patters">
+        The summary of React Patterns
       </Collapsible>
     </div>
   );
--- src/components/Collapsible.tsx
+++ src/components/Collapsible.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 type CollapsableProps = {
     title: string;
@@ -6,11 +6,17 @@ type CollapsableProps = {
 
 }
 
+
 const Collapsible: React.FC<CollapsableProps> = ({ title, children }) => {
+  const [showChildren, setshowChildren] = useState(true);
+
+const handleHideChildren = () => {
+  setshowChildren((prevShowChildren) => !prevShowChildren);
+};
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '20px' }}>
-          {title}<br></br>
-          {children}
+          <h2 onClick={handleHideChildren}>{title}</h2><br></br>
+          {showChildren && children } 
         </div>
       );
     };
